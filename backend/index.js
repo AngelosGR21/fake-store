bc7f420642cd37d65a5c29b98d5fd5a79cbf2a6a
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-12-07 23:36:14 +0000

    adding database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DBID,
+});
+// console.log(connection);
+connection.query("SELECT * FROM `test`", (err, results, fields) => {
+  console.log(results); // results contains rows returned by server
+  // console.log(fields); // fields contains extra meta data about results, if available
+});
 
 app.get("/", (req, res) => {
-  res.send("Testing");
+  res.json({ testing: "Test" });
 });
 
 app.listen(3000, () => {
