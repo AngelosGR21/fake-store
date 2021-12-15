ce2dbba6547caaaa80b1efed33d8709616436484
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-12-15 00:02:09 +0000

    updating frontend with mui and react router dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
