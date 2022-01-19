032bde6ba3d2faa1ae648148bc44756614ab21f5
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2022-01-19 00:35:38 +0000

    adding verification function with api request

--- frontend/src/utils/usersAPI.js
+++ frontend/src/utils/usersAPI.js
@@ -2,14 +2,57 @@ import axios from "axios";
 
 const createUser = async (details) => {
   try {
-    await axios.post("http://localhost:5000/api/signup", {
-      ...details,
+    let data = await axios.post("http://localhost:5000/api/signup", details, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
     });
-    return "success";
+    return data.data;
   } catch (e) {
+    let response = e.response.data;
+    return response;
+  }
+};
+
+const loginUser = async (details) => {
+  try {
+    const data = await axios.post("http://localhost:5000/api/login", details, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token"),
+      },
+      withCredentials: true,
+    });
+    localStorage.setItem("token", data.data.token);
+    return data.data;
+  } catch (e) {
+    return false;
+  }
+};
+
+const fetchUsers = async () => {
+  try {
+    const data = await axios.get("http://localhost:5000/api/users", {
+      headers: { "x-access-token": localStorage.getItem("token") },
+    });
+    return data.data.data;
+  } catch (e) {
+    console.log(e.response);
     let responseMessage = e.response.data.message;
     return responseMessage;
   }
 };
 
-export { createUser };
+const getUserDetails = async (state) => {
+  try {
+    const data = await axios.get("http://localhost:5000/api/userInfo", {
+      headers: { "x-access-token": localStorage.getItem("token") },
+    });
+    return data.data;
+  } catch (e) {
+    return e.response.data;
+  }
+};
+
+export { createUser, loginUser, fetchUsers, getUserDetails };
