0cbc2e6f7d423da4d2627005d59888cdd5ef6112
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2022-01-19 00:46:56 +0000

    adding token verification endpoint

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const { connection, dbQuery } = require("./database");
 const uid = require("uid").uid;
 const bcrypt = require("bcrypt");
 
-//testing cookies
+//jwt
 const jwt = require("jsonwebtoken");
 //middlewares
 app.use(
@@ -24,23 +24,6 @@ app.use(
 app.use(express.json());
 connection.query(`USE ${process.env.DB_DBID}`);
 
-const verifyAdmin = (req, res, next) => {
-  let token = req.headers["x-access-token"];
-  let user = jwt.verify(`${token}i`, process.env.SECRET, (err, decode) => {
-    if (err) {
-      return false;
-    }
-    return decode.isAdmin;
-  });
-  if (user) {
-    next();
-  } else {
-    return res.status(403).json({
-      request: false,
-      message: "You are not an admin",
-    });
-  }
-};
 app.get("/api/products", async (req, res) => {
   try {
     let data = await dbQuery("SELECT * FROM `products`");
@@ -73,62 +56,79 @@ app.get("/api/products/:category");
 
 //creating user !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 app.post("/api/signup", async (req, res) => {
-  //destructuring user details
-  const { username, password, firstName, surname, email } = req.body;
-  //if all details are available create a new user
-  if ((username, password, firstName, surname, email)) {
-    //checking if username is already being used
-    let usernameCheck = await dbQuery(
-      `SELECT * FROM users WHERE username = '${username}'`
-    );
-    //checking if email is already being used
-    let emailCheck = await dbQuery(
-      `SELECT * FROM users WHERE email = '${email}'`
-    );
-    //if the length is  0 = username is available else username is not available
-    if (!usernameCheck[0].length && !emailCheck[0].length) {
-      //hashing password so we can store it in the db
-      let hash = await bcrypt.hash(password, 15);
-      //querying into the db and creating user
-      await dbQuery(`INSERT INTO users (id,firstName, surname, email, username, password, isAdmin)
-      VALUES ('${uid()}','${firstName}', '${surname}', '${email}', '${username}', '${hash}', false )`);
-      let userCreated = await dbQuery(
+  try {
+    //destructuring user details
+    const { username, password, firstName, surname, email } = req.body;
+    //if all details are available create a new user
+    if ((username, password, firstName, surname, email)) {
+      //checking if username is already being used
+      let usernameCheck = await dbQuery(
         `SELECT * FROM users WHERE username = '${username}'`
       );
-      res.status(201).json({
-        request: "success",
-        id: userCreated[0][0].id,
-        isAdmin: userCreated[0][0].isAdmin,
-        username: userCreated[0][0].username,
-      });
+      //checking if email is already being used
+      let emailCheck = await dbQuery(
+        `SELECT * FROM users WHERE email = '${email}'`
+      );
+      //if the length is  0 = username is available else username is not available
+      if (!usernameCheck[0].length && !emailCheck[0].length) {
+        //hashing password so we can store it in the db
+        let hash = await bcrypt.hash(password, 15);
+        //querying into the db and creating user
+        await dbQuery(`INSERT INTO users (id,firstName, surname, email, username, password, isAdmin)
+        VALUES ('${uid()}','${firstName}', '${surname}', '${email}', '${username}', '${hash}', false )`);
+        let userCreated = await dbQuery(
+          `SELECT * FROM users WHERE username = '${username}'`
+        );
+        //creating token
+        let token = jwt.sign(
+          {
+            isAdmin: checkUser[0][0].isAdmin,
+            id: checkUser[0][0].id,
+            username: checkUser[0][0].username,
+            firstName: checkUser[0][0].firstName,
+            surname: checkUser[0][0].surname,
+          },
+          process.env.SECRET,
+          { algorithm: "HS256" }
+        );
+        res.status(201).json({
+          request: true,
+          id: userCreated[0][0].id,
+          isAdmin: userCreated[0][0].isAdmin,
+          username: userCreated[0][0].username,
+          token,
+        });
+      } else {
+        //if username and email are in use
+        if (usernameCheck[0].length && emailCheck[0].length) {
+          return res.status(406).json({
+            request: false,
+            message: "Username and email are already being used",
+          });
+        }
+        //if only username is in use
+        if (usernameCheck[0].length) {
+          return res.status(406).json({
+            request: false,
+            message: "Username is taken",
+          });
+        }
+        //if only email is in use
+        if (emailCheck[0].length) {
+          return res.status(406).json({
+            request: false,
+            message: "Email is already being used",
+          });
+        }
+      }
     } else {
-      //if username and email are in use
-      if (usernameCheck[0].length && emailCheck[0].length) {
-        return res.status(406).json({
-          request: "failed",
-          message: "Username and email are already being used",
-        });
-      }
-      //if only username is in use
-      if (usernameCheck[0].length) {
-        return res.status(406).json({
-          request: "failed",
-          message: "Username is taken",
-        });
-      }
-      //if only email is in use
-      if (emailCheck[0].length) {
-        return res.status(406).json({
-          request: "failed",
-          message: "Email is already being used",
-        });
-      }
+      res.status(500).json({
+        request: "failed",
+        message: "Something went wrong, please try again later",
+      });
     }
-  } else {
-    res.status(500).json({
-      request: "failed",
-      message: "Something went wrong, please try again later",
-    });
+  } catch (e) {
+    console.log(e);
   }
 }); //creating user !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -148,7 +148,13 @@ app.post("/api/login", async (req, res) => {
       if (result) {
         //send jwt token
         let token = jwt.sign(
-          { isAdmin: checkUser[0][0].isAdmin, id: checkUser[0][0].id },
+          {
+            isAdmin: checkUser[0][0].isAdmin,
+            id: checkUser[0][0].id,
+            username: checkUser[0][0].username,
+            firstName: checkUser[0][0].firstName,
+            surname: checkUser[0][0].surname,
+          },
           process.env.SECRET,
           {
             algorithm: "HS256",
@@ -176,8 +182,41 @@ app.post("/api/login", async (req, res) => {
   }
 }); // logging user in !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+//user verification and data sending endpoint
+app.get("/api/userInfo", (req, res) => {
+  let token = req.headers["x-access-token"];
+  //if token was found
+  if (token !== "null") {
+    //verifying token
+    let user = jwt.verify(token, process.env.SECRET, (err, decode) => {
+      if (err) {
+        return false;
+      }
+      //if the token passed verification return the user details
+      return decode;
+    });
+    //if token passed verification send the details
+    if (user) {
+      return res.status(200).json({
+        request: true,
+        userDetails: user,
+      });
+    } else {
+      return res.status(400).json({
+        request: false,
+        message: "Token is invalid",
+      });
+    }
+  }
+  //return false as token didn't pass verification
+  return res.status(404).json({
+    request: false,
+    message: "Token was not found",
+  });
+});
+
 //admin panel
-app.get("/api/users", verifyAdmin, async (req, res) => {
+app.get("/api/users", async (req, res) => {
   let results = await dbQuery("SELECT * FROM users");
   for (let i = 0; i < results[0].length; i++) {
     delete results[0][i].password;
