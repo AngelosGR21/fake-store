ef45857ca916997cf0dfe78a46f7b160dd68dc47
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2022-02-05 23:13:11 +0000

    adding middleware functions

---
+++ backend/middleware.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const ApiError = require("./utils/error");
+
+const verifyUser = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (token !== "null") {
+    //verifying token
+    let user = jwt.verify(token, process.env.SECRET, (err, decode) => {
+      if (err) {
+        return false;
+      }
+      //if the token passed verification return the user details
+      return decode;
+    });
+    //if token passed verification give access to route
+    if (user) {
+      next();
+      return;
+    } else {
+      next(new ApiError(400, "Token is invalid"));
+      return;
+    }
+  }
+  //return false as token didn't pass verification
+  next(new ApiError(404, "Token was not found"));
+  return;
+};
+
+const isAdmin = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (token !== undefined) {
+    let user = jwt.verify(token, process.env.SECRET, (err, decode) => {
+      if (err) {
+        return err;
+      }
+      return decode;
+    });
+    console.log(user);
+    //if user is an admin, next
+    if (user.isAdmin) {
+      return next();
+    } else {
+      //user not admin, throw error
+      next(new ApiError(401, "Unauthorized"));
+      return;
+    }
+  } else {
+    next(new ApiError(404, "token not found"));
+    return;
+  }
+};
+
+module.exports = { verifyUser, isAdmin };
