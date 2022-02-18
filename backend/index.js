4382cbffd0f072c904221a4f3ccd0729d7550340
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2022-02-18 23:15:15 +0000

    adding PUT request on CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT"],
   })
 );
 app.use(express.json());
